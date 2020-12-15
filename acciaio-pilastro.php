183c7f29f4757e211af3214b161ffc1d4e4da41d
pryons <67062313+pryons@users.noreply.github.com>
2020-12-15 12:38:11 +0100

    aggiornamento con formulazioni NTC2018 e circolare

--- acciaio-pilastro.php
+++ acciaio-pilastro.php
@@ -1,3 +1,4 @@
+<?php
     public static function defaultData($data) {
         return [
             'note' => '',
@@ -7,7 +8,7 @@
             'sigla' => '',
             'tipo' => '',
             'acciaio' => '',
-            'gammam' => '',
+            'gammaM' => '',
             'jx' => '',
             'wx' => '',
             'wxp' => '',
@@ -37,7 +38,11 @@
             'Msdx' => '',
             'Msdy' => '',
             'Meqx' => '',
-            'Meqy' => ''
+            'Meqy' => '',
+            'MxA' => '',
+            'MyA' => '',
+            'MxB' => '',
+            'MyB' => '',
         ];
     }
 
@@ -82,6 +87,10 @@
             'Msdy' => $data["Msdy"],
             'Meqx' => $data["Meqx"],
             'Meqy' => $data["Meqy"],
+            'MxA' => $data["MxA"],
+            'MyA' => $data["MyA"],
+            'MxB' => $data["MxB"],
+            'MyB' => $data["MyB"],
         ];
     }
 
@@ -188,6 +197,7 @@
         }
         
         $eps = sqrt ( 235 / $data['fyk'] );
+        //flessione intorno a x
         $psiClasse = (( $data["Ned"] / $data["Atot"] )-( $data['Msdx'] / $data['wx'] )) / (( $data["Ned"] / $data["Atot"] ) + ( $data['Msdx'] / $data['wx'] ));
         if ( $psiClasse > 0 ){
             $alfaClasse = 1;
@@ -205,6 +215,25 @@
             $limiteCL3 = (42*$eps)/(0.67+(0.33*$psiClasse));
         }else{
             $limiteCL3 = (62*$eps)*(1-$psiClasse)*sqrt(-$psiClasse);
+        }
+        //flessione intorno a y
+        $psiClassex = (( $data["Ned"] / $data["Atot"] )-( $data['Msdy'] / $data['wy'] )) / (( $data["Ned"] / $data["Atot"] ) + ( $data['Msdy'] / $data['wy'] ));
+        if ( $psiClassex > 0 ){
+            $alfaClassex = 1;
+        }else{
+            $alfaClassex = 1 / ( - $psiClassex + 1 );
+        }
+        if ( $alfaClassex > 0.5 ) {
+            $limiteCL1x = (396*$eps)/(13*$alfaClassex - 1);
+            $limiteCL2x = (456*$eps)/(13*$alfaClassex - 1);
+        }else{
+            $limiteCL1x = (36*$eps)/$alfaClassex;
+            $limiteCL2x = (41.5*$eps)/$alfaClassex;
+        }
+        if ( $psiClassex > -1 ) {
+            $limiteCL3x = (42*$eps)/(0.67+(0.33*$psiClassex));
+        }else{
+            $limiteCL3x = (62*$eps)*(1-$psiClassex)*sqrt(-$psiClassex);
         }
         
         if (($tipoprofilato == 'TT') OR ($tipoprofilato == 'PC')) {  //tubolari tondi a freddo o caldo
@@ -239,11 +268,11 @@
             }else{
                 $classesez1 = '4';
             }            
-            if ( $verCL2 <= ($limiteCL1) ) {
+            if ( $verCL2 <= ($limiteCL1x) ) {
                 $classesez2 = '1';
-            }elseif ( $verCL2 <= ($limiteCL2) ) {
+            }elseif ( $verCL2 <= ($limiteCL2x) ) {
                 $classesez2 = '2';
-            }elseif ( $verCL2 <= ($limiteCL3) ) {
+            }elseif ( $verCL2 <= ($limiteCL3x) ) {
                 $classesez2 = '3';
             }else{
                 $classesez2 = '4';
@@ -348,18 +377,24 @@
                     $Aclasse = Inge_Function::getWeffOmega($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["s_prof"], $eps)['AreaTot']/100;
                     $jxclasse = Inge_Function::getWeffOmega($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["s_prof"], $eps)['jxeff'];
                     $jyclasse = Inge_Function::getWeffOmega($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["s_prof"], $eps)['jyeff'];
+                    $eY = Inge_Function::getWeffOmega($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["s_prof"], $eps)['ygc']/1000; //in m
+                    $eX = Inge_Function::getWeffOmega($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["s_prof"], $eps)['xgc']/1000; //in m
                 }elseif (($tipoprofilato=='TQ')OR($tipoprofilato=='TR')OR($tipoprofilato=='PQ')OR($tipoprofilato=='PR')) {
                     $wxclasse = Inge_Function::getWeffTubi($data["h_prof"], $data["b_prof"], $data["s_prof"], $eps)['wxeff']/1000;
                     $wyclasse = Inge_Function::getWeffTubi($data["h_prof"], $data["b_prof"], $data["s_prof"], $eps)['wyeff']/1000;
                     $Aclasse = Inge_Function::getWeffTubi($data["h_prof"], $data["b_prof"], $data["s_prof"], $eps)['AreaTot']/100;
                     $jxclasse = Inge_Function::getWeffTubi($data["h_prof"], $data["b_prof"], $data["s_prof"], $eps)['jxeff'];
                     $jyclasse = Inge_Function::getWeffTubi($data["h_prof"], $data["b_prof"], $data["s_prof"], $eps)['jyeff'];
+                    $eY = Inge_Function::getWeffTubi($data["h_prof"], $data["b_prof"], $data["s_prof"], $eps)['ygc']/1000; //in m
+                    $eX = Inge_Function::getWeffTubi($data["h_prof"], $data["b_prof"], $data["s_prof"], $eps)['xgc']/1000; //in m
                 }elseif (($tipoprofilato=='IP')OR($tipoprofilato=='HE')) {
                     $wxclasse = Inge_Function::getWeffIeH($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["e_prof"], $data["s_prof"], $eps)['wxeff']/1000;
                     $wyclasse = Inge_Function::getWeffIeH($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["e_prof"], $data["s_prof"], $eps)['wyeff']/1000;
                     $Aclasse = Inge_Function::getWeffIeH($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["e_prof"], $data["s_prof"], $eps)['AreaTot']/100;
                     $jxclasse = Inge_Function::getWeffIeH($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["e_prof"], $data["s_prof"], $eps)['jxeff'];
                     $jyclasse = Inge_Function::getWeffIeH($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["e_prof"], $data["s_prof"], $eps)['jyeff'];
+                    $eY = Inge_Function::getWeffIeH($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["e_prof"], $data["s_prof"], $eps)['ygc']/1000; //in m
+                    $eX = Inge_Function::getWeffIeH($data["h_prof"], $data["b_prof"], $data["r_prof"], $data["e_prof"], $data["s_prof"], $eps)['xgc']/1000; //in m
                 }else{
                     $data['alert']='ERRsezione4';
                     $wxclasse = '';
@@ -428,6 +463,8 @@
         $chimin=min($chix,$chiy);
         $nMN = $data["Ned"]/$Nrd;        
         if (($tipoprofilato == 'IP')OR($tipoprofilato == 'HE')){
+            $alfaEXP = 2;
+            $betaEXP = min(1,5*$nMN);
             if ($data["h_prof"]/$data["b_prof"] > 2) $curva = 'c';
             else $curva = 'b';
             $datajy = $data['jy']; 
@@ -439,15 +476,19 @@
                 $Mnrdy = $Mrdy;
             }else{
                 $Mnrdy = $Mrdy*(1-pow(($nMN-$aMN)/(1-$aMN),2));
-            }
+            }            
         }elseif(($tipoprofilato == 'TT')OR($tipoprofilato == 'PC')){
+            $alfaEXP = 2;
+            $betaEXP = 2;
             $curva = 'd';
             $datajy = $data['jy']; 
             $datajt = pi()*( pow($data['b_prof'],4)-pow(($data['b_prof']-2*$data['s_prof']),4) )/32; 
             $datajw = 0;
-            $Mnrdx = 1.04*$Mrdx*(1-pow($nMN,1.7));
+            $Mnrdx = $Mrdx*(1-pow($nMN,1.7));
             $Mnrdy = $Mnrdx;
         }elseif(($tipoprofilato == 'TQ')OR($tipoprofilato == 'PQ')OR($tipoprofilato == 'TR')OR($tipoprofilato == 'PR')){
+            $alfaEXP = min((1.66/(1-(1.13*pow($nMN,2)))),6);
+            $betaEXP = $alfaEXP;
             $curva = 'd';
             $datajy = $data['jy']; 
             $datajt = 4*pow( ($data['b_prof']-$data['s_prof'])*($data['h_prof']-$data['s_prof']) ,2)*$data['s_prof'] / ( 2*($data['b_prof']-$data['s_prof'])+2*($data['h_prof']-$data['s_prof'])); 
@@ -456,11 +497,13 @@
             $aMNe = ($data["Atot"]-2*$data["h_prof"]/10*$data["s_prof"]/10)/$data["Atot"];
             $Mnrdx = ($Mrdx*(1-$nMN)/(1-0.5*$axMNs));
             $Mnrdy = ($Mrdx*(1-$nMN)/(1-0.5*$axMNe));
-        }elseif($tipoprofilato == 'UPN'){          // PROFILATO UPN - nessun riferimento normativo - ipotesi da formulare
+        }elseif($tipoprofilato == 'UP'){          // PROFILATO UPN - nessun riferimento normativo - ipotesi da formulare
             $curva = 'c';
             $datajy = $data['jy'];
             $datajt = $data['jt'];
-            $datajw = $data['jw'];   
+            $datajw = $data['jw']; 
+            $Mnrdy = $Mrdy;
+            $Mnrdx = $Mrdx;
         }
         
         $alfa = array(
@@ -485,14 +528,51 @@
         $psiLT = 0.5*(1+$alfaLT*($lambdaLT-$lambdaLT0)+$beta*pow($lambdaLT,2));
         $chiLT = (1/$f)*(1 / ($psiLT+sqrt(pow($psiLT,2)-($beta*pow($lambdaLT,2)))));
         $chiLT = min ($chiLT,$kappachi);
- 
-//per le verifiche di resistenza e di stabilità devo implementare formulazioni per classe 3 e 4, secondo EC3 e circolare 7/2019.
-        
+        //per classe 4
+        if ( abs($data['MxA']) >= abs($data['MxB']) ){
+            $Mxmag = $data['MxA'];
+            $Mxmin = $data['MxB'];
+        }else{
+            $Mxmag = $data['MxB'];
+            $Mxmin = $data['MxA'];            
+        }
+        if ( abs($data['MyA']) >= abs($data['MyB']) ){
+            $Mymag = $data['MyA'];
+            $Mymin = $data['MyB'];
+        }else{
+            $Mymag = $data['MyB'];
+            $Mymin = $data['MyA'];
+        }
+        $psiX = $Mxmin/$Mxmag;
+        $psiY = $Mymin/$Mymag;
+        $alfaMX = max(0.6+0.4*$psiX,0.4*$psiX);
+        $alfaMY = max(0.6+0.4*$psiY,0.4*$psiY);
+        $alfaMLT = $alfaMY;
+        if ($classesez == 4){
+            $kXX = min( $alfaMX*(1+(0.6*$lambdax*$data['Ned']*$data['gammaM']/($chix*$Aclasse*$data['fyk']))),$alfaMX*(1+(0.6*$data['Ned']*$data['gammaM']/($chix*$Aclasse*$data['fyk']))) );
+            $kYY = min( $alfaMY*(1+(0.6*$lambday*$data['Ned']*$data['gammaM']/($chiy*$Aclasse*$data['fyk']))),$alfaMY*(1+(0.6*$data['Ned']*$data['gammaM']/($chiy*$Aclasse*$data['fyk']))) );
+            $kXY = $kYY;
+            if ( $tipoprofilato != 'UP' ){
+                $kYX = ($data['Msdy'] == 0) ? 0 : 0.8*$kXX;
+            }else{
+                $kYX = max( 1-((0.05*$lambday/($alfaMLT-0.25))*($data['Ned']*$data['gammaM']/($chiy*$Aclasse*$data['fyk']))),1-((0.05/($alfaMLT-0.25))*($data['Ned']*$data['gammaM']/($chiy*$Aclasse*$data['fyk']))) );
+            }
+        }
         //verifica resistenza
         $maggminR = '<';
         $classR = 'success';
         $risR = '';
-        $verRes = ($data['Msdx']/$Mnrdx)+($data['Msdy']/$Mnrdy);
+        if ( $classesez < 3 ){
+            if ( ($tipoprofilato != 'UP')AND($nMN >= 0.2) ){
+                $verRes = pow(($data['Msdx']/$Mnrdx),$alfaEXP)+pow(($data['Msdy']/$Mnrdy),$betaEXP);
+            }else{
+                $verRes = ($data['Msdx']/$Mnrdx)+($data['Msdy']/$Mnrdy);
+            }
+        }elseif ( $classesez == 3 ){
+            $verRes = ($data["Ned"]/$Nrd)+($data['Msdx']/$Mrdx)+($data['Msdy']/$Mrdy);
+        }elseif ( $classesez = 4 ){
+            $verRes = ($data["Ned"]/$Nrd)+(($data['Msdx']+($data["Ned"]*$eY))/$Mrdx)+(($data['Msdy']+($data["Ned"]*$eX))/$Mrdy);
+        }
         if ($verRes > 1){
             $maggminR = '>';
             $classR = 'danger';
@@ -503,12 +583,23 @@
         $classS = 'success';
         $risS = '';
         $risS0 = '<b><i class="fa fa-check-circle"></i></b>';
-        $verStab = ($data["Ned"]*1.05/($chimin*$data["Atot"]*$data['fyk']/10))+($data["Meqx"]*100*1.05/($chiLT*$data['fyk']/10*$wxclasse*(1-($data["Ned"]/$caricoCRx))))+($data["Meqy"]*100*1.05/($data['fyk']/10*$wyclasse*(1-($data["Ned"]/$caricoCRy))));
-        if ($verStab > 1){
-            $maggminS = '>';
-            $classS = 'danger';
-            $risS = 'NON';
-            $risS0 = '<b><i class="fa fa-times-circle"></i></b>';
+        if ( $classesez < 4 ){
+            $verStab = ($data["Ned"]*$data['gammaM']/($chimin*$Aclasse*$data['fyk']/10))+($data["Meqx"]*100*$data['gammaM']/($chiLT*$data['fyk']/10*$wxclasse*(1-($data["Ned"]/$caricoCRx))))+($data["Meqy"]*100*$data['gammaM']/($data['fyk']/10*$wyclasse*(1-($data["Ned"]/$caricoCRy))));
+            if ($verStab > 1){
+                $maggminS = '>';
+                $classS = 'danger';
+                $risS = 'NON';
+                $risS0 = '<b><i class="fa fa-times-circle"></i></b>';
+            }
+        }else{
+            $verStab1 = ($data["Ned"]*$data['gammaM']/($chix*$Aclasse*$data['fyk']/10))+$kXX*(($data['Msdx']+($data["Ned"]*$eY))*100*$data['gammaM']/($chiLT*$data['fyk']/10*$wxclasse))+$kXY*(($data['Msdy']+($data["Ned"]*$eX))*100*$data['gammaM']/($data['fyk']/10*$wyclasse));
+            $verStab2 = ($data["Ned"]*$data['gammaM']/($chiy*$Aclasse*$data['fyk']/10))+$kYX*(($data['Msdx']+($data["Ned"]*$eY))*100*$data['gammaM']/($chiLT*$data['fyk']/10*$wxclasse))+$kYY*(($data['Msdy']+($data["Ned"]*$eX))*100*$data['gammaM']/($data['fyk']/10*$wyclasse)); 
+            if ( ($verStab1 > 1) OR ($verStab2 > 1) ){
+                $maggminS = '>';
+                $classS = 'danger';
+                $risS = 'NON';
+                $risS0 = '<b><i class="fa fa-times-circle"></i></b>';
+            }
         }
         
         $data['wxclasse'] = round($wxclasse, 0);
@@ -548,3 +639,4 @@
 
         return $data;
     }
+?>
